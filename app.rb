7da0104aad97ab55c6eb053b501fee41a17ff36b
sraaths <sraaths@uchicago.edu>
2024-10-23 16:36:58 +0000

    c1

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
 
+# Route to display the dice form
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:dice_form)
+end
+
+# Route to process the roll
+get("/process_roll") do
+  @num_dice = params.fetch("dice").to_i
+  @num_sides = params.fetch("sides").to_i
+  @rolls = Array.new(@num_dice) { rand(1..@num_sides) }  # Generate dice rolls
+  erb :results
 end
